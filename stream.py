201324200c636c07009d30a65d4000c06bea7c6a
Hardikk011 <hvp5694@gmail.com>
2025-03-13 12:58:28 +0530

    My First Streamlit

---
+++ stream.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("My First Cloud App")
+st.write("A simple DataFtrame: ")
+
+df = pd.DataFrame(np.random.randn(10,2),columns=['col1','col2'])
+st.dataframe(df)
